373dd84652b27adb9a8037a32e266b1bb0a084e4
Mark Liffiton <liffiton@gmail.com>
2024-04-10 14:30:57 -0500

    Remove old/unneeded config entry.

--- app.py
+++ app.py
@@ -21,7 +21,6 @@ app = Flask(__name__)
 # Load default config and override config from an environment variable
 app.config.update(dict(
     DATABASE=os.path.join(app.root_path, 'flaskr.db'),
-    DEBUG=True,
     SECRET_KEY='development key',
 ))
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
